4f698486d63275523ed43caf20c149ca38bc2efe
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-01-15 17:07:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#include <math.h>
 #include "libs/analog.h"
 #include "libs/LCD.h"
 
 int main(void) {
 
+    const uint16_t B = 3.9E3;   // El beta de la formula, en grados Kelvin
+    const uint16_t T0 = 298;    // T0 que es 25 °C pero lo ponemos en Kelvin
+    const uint16_t R = 10E3;    // R(T0)
+    
     while(1) {
 
         // TODO
